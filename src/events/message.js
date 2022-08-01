e866034415799ee6296a8b111f9bffc7aeb2f49b
cmrxnn <cmrxn2005@gmail.com>
2022-08-01 22:21:14 +0100

    fix dj role check

--- src/events/message.js
+++ src/events/message.js
@@ -1,18 +1,13 @@
 'use strict';
 
-const { setLoading, setError } = require("../helpers");
 const config = require('../../config.json');
+const { PermissionsBitField } = require('discord.js');
+const { setLoading, setError } = require("../helpers");
 
 module.exports = {
     exec: async (client, message) => {
         // Some checks to see if it's a valid message
         if (!message.guild || message.author.bot) return;
-
-        if (config.role) {
-            if (!message.member.roles.cache.find(r => r.id === config.role)) {
-                return setError(message, 'You do not have the DJ role.');
-            };
-        };
 
         const prefix = message.content.toLowerCase().startsWith(client.prefix) ? client.prefix : `<@!${client.user.id}> `;
         const [name, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -20,6 +15,12 @@ module.exports = {
         // Get the command and check if it exists
         const command = client.commands.get(name);
         if (!command) return;
+
+        if (config.role) {
+            if (!m.member.permissions.has(PermissionsBitField.Flags.MuteMembers)) {
+                return setError(message, 'You do not have the DJ role.');
+            };
+        };
 
         // Run the command and catch any errors
         try {
